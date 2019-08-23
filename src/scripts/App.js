5192d9ed46366d4bcb79d3686ec2cf0bc0cde226
Gittenger <john.pittenger86@gmail.com>
2019-08-22 20:09:52 -0400

    added error handling to display quote func

--- src/scripts/App.js
+++ src/scripts/App.js
@@ -3,18 +3,22 @@ import React from "react";
 async function displayQuote() {
   const url =
     "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
-  const responseObj = await fetch(url);
-  const data = await responseObj.json();
 
-  const quotes = data.quotes;
-  const index = randNum(quotes.length);
-  const quote = quotes[index];
+  try {
+    const responseObj = await fetch(url);
+    const data = await responseObj.json();
+    const quotes = data.quotes;
+    const index = randNum(quotes.length);
+    const quote = quotes[index];
 
-  const quoteEl = document.querySelector("#quote");
-  const authorEl = document.querySelector("#author");
+    const quoteEl = document.querySelector("#quote");
+    const authorEl = document.querySelector("#author");
 
-  quoteEl.innerHTML = quote.quote;
-  authorEl.innerHTML = quote.author;
+    quoteEl.innerHTML = quote.quote;
+    authorEl.innerHTML = quote.author;
+  } catch (err) {
+    console.error("Error grabbing quote");
+  }
 }
 
 const randNum = max => {
