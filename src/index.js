108401d9a33f4734ac62f51a6760ada313fa781b
John Pittenger <36045366+Gittenger@users.noreply.github.com>
2022-05-06 14:09:21 -0400

    update react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles/main.scss";
-import App from "./scripts/App";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/main.scss'
+import App from './scripts/App'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+)
